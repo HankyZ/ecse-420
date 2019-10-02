ae003207292ca1ae84fa94aab06af4b2ebda26e0
Hank Zhang <yuhuan.zhang@mail.mcgill.ca>
2019-10-02 18:45:28 -0400

    made it work, still not supporting any amount of threads

--- lab1_image_rectification/lab1_image_rectification/kernel.cu
+++ lab1_image_rectification/lab1_image_rectification/kernel.cu
@@ -10,12 +10,12 @@ __global__ void process(unsigned char* image, unsigned char* new_image, unsigned
 
 	for (int a = 0; a < 4; a++)
 	{
-		unsigned value = image[i + a] - 0;
-		//printf("value is %d\n", value);
+		int p = i + a;
+		int value = image[p] - 127;
 		if (value < 0)
-			new_image[i + a] = 0;
+			new_image[p] = 0;
 		else
-			new_image[i + a] = value + 0;
+			new_image[p] = value + 127;
 	}
 }
 
@@ -27,7 +27,7 @@ void printArray(int* a, int n)
 	}
 }
 
-int compareArray(unsigned char *a, unsigned char* b, int size)
+int compareArray(unsigned char* a, unsigned char* b, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -66,52 +66,52 @@ int main()
 	cudaMallocManaged((void**)& shared_image, (unsigned long long)w * (unsigned long long)h * 4 * sizeof(unsigned char));
 	cudaMallocManaged((void**)& new_image, (unsigned long long)w * (unsigned long long)h * 4 * sizeof(unsigned char));
 
-	int counter = 0;
-	for (int i = 0; i < w; i++)
+	for (int i = 0; i < h; i++)
 	{
-		for (int j = 0; j < h; j++)
+		for (int j = 0; j < w; j++)
 		{
 			for (int a = 0; a < 4; a++)
 			{
 				int p = 4 * w * i + 4 * j + a;
-				printf("p = %d\n", p);
-				if (p == 2005984)
-				{
-					printf("image[%d] = %d\n", p, image[p]);
-				}
 				shared_image[p] = image[p];
-				counter++;
 			}
 		}
 	}
 
-	printf("counter is %d\n", counter);
-	printf("h * w * 4 = %d\n", h * w * 4);
+	unsigned char* expected_new_image;
+	expected_new_image = (unsigned char*)malloc((unsigned long long)w * (unsigned long long)h * 4 * sizeof(unsigned char));
 
-	if (compareArray(image, shared_image, w * h * 4) == 0)
-		printf("same\n");
-	else
-		printf("not same\n");
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			for (int a = 0; a < 4; a++)
+			{
+				int p = 4 * w * i + 4 * j + a;
+				int value = image[p] - 127;
 
-	printf("w is %d, h is %d\n", w, h);
+				if (value < 0)
+					expected_new_image[p] = 0;
+				else
+					expected_new_image[p] = value + 127;
+			}
+		}
+	}
 
-	process << <w, h >> > (shared_image, new_image, d_w, d_h);
+	process << <h, w >> > (shared_image, new_image, d_w, d_h);
 
 	cudaDeviceSynchronize();
 
 	lodepng_encode32_file(output_filename, new_image, w, h);
 
-	if (compareArray(image, new_image, w * h * 4) == 0)
+	if (compareArray(expected_new_image, new_image, 4 * w * h) == 0)
 		printf("same\n");
-	else
-		printf("not same\n");
 
 	cudaFree(d_w);
 	cudaFree(d_h);
 	cudaFree(shared_image);
 	cudaFree(new_image);
 	free(image);
-
 
 	return 0;
 }
